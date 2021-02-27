3ae49c8a81503a85b7d01f354b43c998164431b0
Marcel Hernandez <e6990620@gmail.com>
2021-02-27 03:12:32 +0100

    Relax max line length rules and start modelling a few simple concepts.

--- libraytracing/src/basic.adb
+++ libraytracing/src/basic.adb
@@ -3,4 +3,45 @@ package body Basic is
    begin
       return "42";
    end Answer;
+
+   function "+" (A, B : Tuple) return Tuple is
+      C : constant Tuple := (
+         X => A.X + B.X,
+         Y => A.Y + B.Y,
+         Z => A.Z + B.Z,
+         W => A.W + B.W
+      );
+   begin
+      return C;
+   end "+";
+
+   function "-" (A, B : Tuple) return Tuple is
+      C : constant Tuple := (
+         X => A.X - B.X,
+         Y => A.Y - B.Y,
+         Z => A.Z - B.Z,
+         W => A.W - B.W
+      );
+   begin
+      return C;
+   end "-";
+
+   function "-" (A : Tuple) return Tuple is
+      Zero : constant Tuple := (0.0, 0.0, 0.0, 0.0);
+      Negated : constant Tuple := Zero - A;
+   begin
+      return Negated;
+   end "-";
+
+   function New_Point (X, Y, Z : Value) return Tuple is
+      Point : constant Tuple := (X, Y, Z, 1.0);
+   begin
+      return Point;
+   end New_Point;
+
+   function New_Vector (X, Y, Z : Value) return Tuple is
+      Vector : constant Tuple := (X, Y, Z, 0.0);
+   begin
+      return Vector;
+   end New_Vector;
 end Basic;
--- libraytracing/src/basic.ads
+++ libraytracing/src/basic.ads
@@ -1,3 +1,22 @@
 package Basic is
    function Answer return String;
+
+   Value_Epsilon : constant := 0.0001;
+   Min_Value : constant := -1_000_000_000_000.0;
+   Max_Value : constant := 1_000_000_000_000.0;
+   type Value is delta Value_Epsilon range Min_Value .. Max_Value;
+
+   type Tuple is record
+      X : Value := 0.0;
+      Y : Value := 0.0;
+      Z : Value := 0.0;
+      W : Value := 0.0;
+   end record;
+
+   function "+" (A, B : Tuple) return Tuple;
+   function "-" (A, B : Tuple) return Tuple;
+   function "-" (A : Tuple) return Tuple;
+
+   function New_Point (X, Y, Z : Value) return Tuple;
+   function New_Vector (X, Y, Z : Value) return Tuple;
 end Basic;
