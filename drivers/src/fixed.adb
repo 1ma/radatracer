9c86204049e78cb664c59e7cd173844e2d02f624
Marcel Hernandez <e6990620@gmail.com>
2021-03-08 01:51:51 +0100

    evolved the 'fixed' test program to illustrate problem with the Generic_Elementary_Functions package

--- drivers/src/fixed.adb
+++ drivers/src/fixed.adb
@@ -1,12 +1,24 @@
+with Ada.Numerics.Generic_Elementary_Functions;
 with Ada.Text_IO;
 
 procedure Fixed is
-   type MyFixedReal is delta 10.0 ** (-5) digits 18;
+   type MyDigits is digits 10;
+   type MyDeltaRanged is delta 0.01 range -10.0 .. 10.0;
+   type MyDeltaDigits is delta 0.01 digits 10;
 
-   Z : constant MyFixedReal := 0.0;
+   package MD_Ops is new Ada.Numerics.Generic_Elementary_Functions (MyDigits);
+   --  package MDR_Ops is new Ada.Numerics.Generic_Elementary_Functions (MyDeltaRanged);
+   --  package MDD_Ops is new Ada.Numerics.Generic_Elementary_Functions (MyDeltaDigits);
+
+   D : constant MyDigits := 2.0;
+   DR : constant MyDeltaRanged := 2.0;
+   DD : constant MyDeltaDigits := 2.0;
 begin
-   Ada.Text_IO.Put_Line (MyFixedReal'Size'Image);
-   Ada.Text_IO.Put_Line (MyFixedReal'First'Image);
-   Ada.Text_IO.Put_Line (MyFixedReal'Last'Image);
-   Ada.Text_IO.Put_Line (Z'Image);
+   Ada.Text_IO.Put_Line (D'Image);
+   Ada.Text_IO.Put_Line (DR'Image);
+   Ada.Text_IO.Put_Line (DD'Image);
+   Ada.Text_IO.New_Line;
+   Ada.Text_IO.Put_Line (MD_Ops.Sqrt (D)'Image);
+   --  Ada.Text_IO.Put_Line (MDR_Ops.Sqrt (DR)'Image);
+   --  Ada.Text_IO.Put_Line (MDD_Ops.Sqrt (DD)'Image);
 end Fixed;
