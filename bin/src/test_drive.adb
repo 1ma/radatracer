be84dde79752243ac032cb4e35892c9d73580ca0
Marcel Hernandez <e6990620@gmail.com>
2021-04-04 17:37:17 +0200

    turn the test_drive program into an Ada.Text_IO example

--- bin/src/test_drive.adb
+++ bin/src/test_drive.adb
@@ -1,32 +1,15 @@
 with Ada.Text_IO;
-with Radatracer;
-
-use type Radatracer.Tuple;
 
 procedure Test_Drive is
-   A : constant Radatracer.Tuple := (1.0, 1.0, 1.0, 1.0);
-   B : constant Radatracer.Tuple := (1.0, 2.0, 3.0, 0.0);
-   C : constant Radatracer.Tuple := A + B;
-   D : constant Radatracer.Tuple := B - A;
-   E : constant Radatracer.Tuple := -B;
-
-   procedure Print_Tuple (T : Radatracer.Tuple);
-   procedure Print_Tuple (T : Radatracer.Tuple) is
-   begin
-      Ada.Text_IO.Put ("X =>");
-      Ada.Text_IO.Put (T.X'Image);
-      Ada.Text_IO.Put (", Y =>");
-      Ada.Text_IO.Put (T.Y'Image);
-      Ada.Text_IO.Put (", Z =>");
-      Ada.Text_IO.Put (T.Z'Image);
-      Ada.Text_IO.Put (", W =>");
-      Ada.Text_IO.Put (T.W'Image);
-      Ada.Text_IO.New_Line;
-   end Print_Tuple;
+   F : Ada.Text_IO.File_Type;
 begin
-   Print_Tuple (A);
-   Print_Tuple (B);
-   Print_Tuple (C);
-   Print_Tuple (D);
-   Print_Tuple (E);
+   Ada.Text_IO.Create (File => F, Mode => Ada.Text_IO.Out_File);
+   Ada.Text_IO.Put_Line (F, "Wololo");
+
+   Ada.Text_IO.Reset (File => F, Mode => Ada.Text_IO.In_File);
+
+   while not Ada.Text_IO.End_Of_File (F) loop
+      Ada.Text_IO.Put_Line (Ada.Text_IO.Get_Line (F));
+   end loop;
+   Ada.Text_IO.Close (F);
 end Test_Drive;
