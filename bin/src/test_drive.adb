a30b61ef3f01c6a8da52d25d6fddefbc7ae0c948
Marcel Hernandez <e6990620@gmail.com>
2021-04-05 02:11:06 +0200

    simplify sample PPM code

--- bin/src/test_drive.adb
+++ bin/src/test_drive.adb
@@ -1,14 +1,13 @@
 with Ada.Text_IO;
-with Radatracer;
 with Radatracer.Canvas;
 with Radatracer.Canvas.IO;
 
 procedure Test_Drive is
    C : Radatracer.Canvas.Canvas := Radatracer.Canvas.Make_Canvas (3, 3);
 begin
-   C (1, 1) := Radatracer.Canvas.To_Pixel (Radatracer.Make_Color (1.0, 0.0, 0.0));
-   C (2, 2) := Radatracer.Canvas.To_Pixel (Radatracer.Make_Color (0.0, 1.0, 0.0));
-   C (3, 3) := Radatracer.Canvas.To_Pixel (Radatracer.Make_Color (0.0, 0.0, 1.0));
+   C (1, 1) := (255, 0, 0);
+   C (2, 2) := (0, 255, 0);
+   C (3, 3) := (0, 0, 255);
 
    Radatracer.Canvas.IO.Write_PPM (Ada.Text_IO.Standard_Output, C);
 end Test_Drive;
