1ffab899cae8ff4bf7a842818d5c058a7c231790
Marcel Hernandez <e6990620@gmail.com>
2021-04-06 20:28:13 +0200

    start glorious work on matrices

---
+++ lib/src/radatracer-matrices.ads
@@ -0,0 +1,7 @@
+package Radatracer.Matrices is
+   pragma Pure;
+
+   type Matrix2 is array (0 .. 1, 0 .. 1) of Value;
+   type Matrix3 is array (0 .. 2, 0 .. 2) of Value;
+   type Matrix4 is array (0 .. 3, 0 .. 3) of Value;
+end Radatracer.Matrices;
