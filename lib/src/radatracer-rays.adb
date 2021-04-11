63ad3c943f1f8d3a0701cd7035c51beae245fdb5
Marcel Hernandez <e6990620@gmail.com>
2021-04-11 22:49:42 +0200

    fix Radatracer.Rays.Intersect function and add more tests

--- lib/src/radatracer-rays.adb
+++ lib/src/radatracer-rays.adb
@@ -11,13 +11,13 @@ package body Radatracer.Rays is
 
       A : constant Value := 2.0 * Dot_Product (R.Direction, R.Direction);
       B : constant Value := 2.0 * Dot_Product (R.Direction, Sphere_Ray_Vector);
-      C : constant Value := Dot_Product (Sphere_Ray_Vector, Sphere_Ray_Vector);
+      C : constant Value := Dot_Product (Sphere_Ray_Vector, Sphere_Ray_Vector) - 1.0;
 
       Discriminant : constant Value := (B * B) - (2.0 * A * C);
    begin
       if Discriminant < 0.0 then
          declare
-            Empty_Result : Value_Array (1 .. 0);
+            Empty_Result : constant Value_Array (1 .. 0) := (others => <>);
          begin
             return Empty_Result;
          end;
--- tests/src/radatracer-rays-tests.adb
+++ tests/src/radatracer-rays-tests.adb
@@ -4,13 +4,31 @@ with AUnit.Assertions;
 package body Radatracer.Rays.Tests is
    procedure Test_Ray_Calculations (T : in out AUnit.Test_Cases.Test_Case'Class);
    procedure Test_Ray_Calculations (T : in out AUnit.Test_Cases.Test_Case'Class) is
-      R1 : constant Ray := (Origin => Make_Point (2, 3, 4), Direction => Make_Vector (1, 0, 0));
-      R2 : constant Ray := (Origin => Make_Point (0, 0, -5), Direction => Make_Vector (0, 0, 1));
+      S : constant Sphere := (others => <>);
+
+      R : constant Ray := (Origin => Make_Point (2, 3, 4), Direction => Make_Vector (1, 0, 0));
+
+      R1 : constant Ray := (Origin => Make_Point (0, 0, -5), Direction => Make_Vector (0, 0, 1));
+      R2 : constant Ray := (Origin => Make_Point (0, 1, -5), Direction => Make_Vector (0, 0, 1));
+      R3 : constant Ray := (Origin => Make_Point (0, 2, -5), Direction => Make_Vector (0, 0, 1));
+      R4 : constant Ray := (Origin => Make_Point (0, 0, 0), Direction => Make_Vector (0, 0, 1));
+      R5 : constant Ray := (Origin => Make_Point (0, 0, 5), Direction => Make_Vector (0, 0, 1));
+      I1 : constant Value_Array := (4.0, 6.0);
+      I2 : constant Value_Array := (5.0, 5.0);
+      I3 : constant Value_Array (1 .. 0) := (others => <>);
+      I4 : constant Value_Array := (-1.0, 1.0);
+      I5 : constant Value_Array := (-6.0, -4.0);
    begin
-      AUnit.Assertions.Assert (Position (R1, 0.0) = Make_Point (2, 3, 4), "Ray position test 1");
-      AUnit.Assertions.Assert (Position (R1, 1.0) = Make_Point (3, 3, 4), "Ray position test 2");
-      AUnit.Assertions.Assert (Position (R1, -1.0) = Make_Point (1, 3, 4), "Ray position test 3");
-      AUnit.Assertions.Assert (Position (R1, 2.5) = Make_Point (4.5, 3.0, 4.0), "Ray position test 4");
+      AUnit.Assertions.Assert (Position (R, 0.0) = Make_Point (2, 3, 4), "Ray position test 1");
+      AUnit.Assertions.Assert (Position (R, 1.0) = Make_Point (3, 3, 4), "Ray position test 2");
+      AUnit.Assertions.Assert (Position (R, -1.0) = Make_Point (1, 3, 4), "Ray position test 3");
+      AUnit.Assertions.Assert (Position (R, 2.5) = Make_Point (4.5, 3.0, 4.0), "Ray position test 4");
+
+      AUnit.Assertions.Assert (Intersect (S, R1) = I1, "Ray-Sphere intersection test 1 - intersection at 2 points");
+      AUnit.Assertions.Assert (Intersect (S, R2) = I2, "Ray-Sphere intersection test 2 - intersection at 1 point (tangent point)");
+      AUnit.Assertions.Assert (Intersect (S, R3) = I3, "Ray-Sphere intersection test 3 - no intersection");
+      AUnit.Assertions.Assert (Intersect (S, R4) = I4, "Ray-Sphere intersection test 4 - ray originates inside the sphere");
+      AUnit.Assertions.Assert (Intersect (S, R5) = I5, "Ray-Sphere intersection test 5 - sphere is behind the ray");
    end Test_Ray_Calculations;
 
    overriding procedure Register_Tests (T : in out Test) is
