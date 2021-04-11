cd5339de3d026c7d9f1834c52466a2c190e71e00
Marcel Hernandez <e6990620@gmail.com>
2021-04-11 15:09:13 +0200

    Try a bit harder not to use Vectors just yet.
    Using the Ada.Containers.Vector package the build time of the project
    increased significantly, and libradatracer.a doubled in size. Not an
    unbearable sacrifice, but also not a good sign.

--- lib/src/radatracer-rays.adb
+++ lib/src/radatracer-rays.adb
@@ -6,21 +6,32 @@ package body Radatracer.Rays is
       return R.Origin + R.Direction * T;
    end Position;
 
-   function Intersect (S : Sphere; R : Ray) return Value_Vectors.Vector is
-      package Math is new Ada.Numerics.Generic_Elementary_Functions (Value);
+   function Intersect (S : Sphere; R : Ray) return Value_Array is
+      Sphere_Ray_Vector : constant Tuple := R.Origin - S.Origin;
 
-      Sphere_To_Ray : constant Tuple := S.Origin - Make_Point (0.0, 0.0, 0.0);
-      A : constant Value := Dot_Product (R.Direction, R.Direction);
-      B : constant Value := 2.0 * Dot_Product (R.Direction, Sphere_To_Ray);
-      C : constant Value := Dot_Product (Sphere_To_Ray, Sphere_To_Ray);
-      Discriminant : constant Value := (B * B) - (4.0 * A * C);
-      Result : Value_Vectors.Vector;
+      A : constant Value := 2.0 * Dot_Product (R.Direction, R.Direction);
+      B : constant Value := 2.0 * Dot_Product (R.Direction, Sphere_Ray_Vector);
+      C : constant Value := Dot_Product (Sphere_Ray_Vector, Sphere_Ray_Vector);
+
+      Discriminant : constant Value := (B * B) - (2.0 * A * C);
    begin
-      if Discriminant >= 0.0 then
-         Result.Append ((-B - Math.Sqrt (Discriminant)) / (2.0 * A));
-         Result.Append ((-B + Math.Sqrt (Discriminant)) / (2.0 * A));
+      if Discriminant < 0.0 then
+         declare
+            Empty_Result : Value_Array (1 .. 0);
+         begin
+            return Empty_Result;
+         end;
       end if;
 
-      return Result;
+      declare
+         package Math is new Ada.Numerics.Generic_Elementary_Functions (Value);
+
+         Normal_Result : constant Value_Array := (
+            (-B - Math.Sqrt (Discriminant)) / A,
+            (-B + Math.Sqrt (Discriminant)) / A
+         );
+      begin
+         return Normal_Result;
+      end;
    end Intersect;
 end Radatracer.Rays;
--- lib/src/radatracer-rays.ads
+++ lib/src/radatracer-rays.ads
@@ -1,21 +1,16 @@
-with Ada.Containers.Vectors;
-
 package Radatracer.Rays is
-   package Value_Vectors is new Ada.Containers.Vectors (
-      Index_Type => Natural,
-      Element_Type => Value
-   );
-
    type Ray is record
       Origin : Tuple;
       Direction : Tuple;
    end record;
 
    type Sphere is record
-      Origin : Tuple;
+      Origin : Tuple := Make_Point (0.0, 0.0, 0.0);
    end record;
+
+   type Value_Array is array (Natural range <>) of Value;
 
    function Position (R : Ray; T : Value) return Tuple;
 
-   function Intersect (S : Sphere; R : Ray) return Value_Vectors.Vector;
+   function Intersect (S : Sphere; R : Ray) return Value_Array;
 end Radatracer.Rays;
