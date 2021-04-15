8e1a4f33b014dfeaf6b6dd514130165c9a3ceb00
Marcel Hernandez <e6990620@gmail.com>
2021-04-15 02:15:20 +0200

    Implemented capstone project of chapter 5.
    Currently the program is segfaulting for "large" images.
    The biggest image that works is 1670x1670. 1671 segfaults.

---
+++ bin/src/silhouette.adb
@@ -0,0 +1,54 @@
+with Ada.Text_IO;
+with Radatracer.Canvas.IO;
+with Radatracer.Matrices;
+with Radatracer.Objects;
+
+use type Radatracer.Tuple;
+use type Radatracer.Value;
+use type Radatracer.Matrices.Matrix4;
+use type Radatracer.Objects.Intersection_Vectors.Cursor;
+
+procedure Silhouette is
+   Ray_Origin : constant Radatracer.Tuple := Radatracer.Make_Point (0, 0, -5);
+
+   Canvas_Pixels : constant := 1670;
+
+   Wall_Z : constant Radatracer.Value := 10.0;
+   Wall_Size : constant Radatracer.Value := 7.0;
+   Half_Wall : constant Radatracer.Value := Wall_Size / 2.0;
+
+   Pixel_Size : constant Radatracer.Value := Wall_Size / Radatracer.Value (Canvas_Pixels);
+
+   World_X, World_Y : Radatracer.Value;
+
+   Ray : Radatracer.Ray;
+   Sphere : constant Radatracer.Objects.Sphere := (
+      Inverted_Transformation => Radatracer.Matrices.Invert (
+         Radatracer.Matrices.Shearing (1.0, 0.0, 0.0, 0.0, 0.0, 0.0) * Radatracer.Matrices.Scaling (0.5, 1.0, 1.0)
+      )
+   );
+
+   Hit : Radatracer.Objects.Intersection_Vectors.Cursor;
+
+   Canvas : Radatracer.Canvas.Canvas (0 .. Canvas_Pixels - 1, 0 .. Canvas_Pixels - 1);
+begin
+   for Y in Canvas'Range (2) loop
+      World_Y := Half_Wall - Pixel_Size * Radatracer.Value (Y);
+
+      for X in Canvas'Range (1) loop
+         World_X := -Half_Wall + Pixel_Size * Radatracer.Value (X);
+
+         Ray := (
+            Origin => Ray_Origin,
+            Direction => Radatracer.Normalize (Radatracer.Make_Point (World_X, World_Y, Wall_Z) - Ray_Origin)
+         );
+
+         Hit := Radatracer.Objects.Hit (Radatracer.Objects.Intersect (Sphere, Ray));
+         if Hit /= Radatracer.Objects.Intersection_Vectors.No_Element then
+            Canvas (X, Y) := Radatracer.Canvas.Red_Pixel;
+         end if;
+      end loop;
+   end loop;
+
+   Radatracer.Canvas.IO.Write_PPM (Ada.Text_IO.Standard_Output, Canvas);
+end Silhouette;
