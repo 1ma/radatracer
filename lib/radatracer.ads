c1c5116680814bed69f557f3a0faf338afea2105
Marcel Hernandez <e6990620@gmail.com>
2021-04-16 00:40:17 +0200

    Added a bunch of preconditions, postconditions and predicates some types and functions.

--- lib/radatracer.ads
+++ lib/radatracer.ads
@@ -26,12 +26,16 @@ package Radatracer is
    function "/" (L : Tuple; R : Value) return Tuple;
 
    function Is_Point (T : Tuple) return Boolean;
-   function Make_Point (X, Y, Z : Value) return Tuple;
-   function Make_Point (X, Y, Z : Integer) return Tuple;
+   function Make_Point (X, Y, Z : Value) return Tuple
+      with Post => Is_Point (Make_Point'Result);
+   function Make_Point (X, Y, Z : Integer) return Tuple
+      with Post => Is_Point (Make_Point'Result);
 
    function Is_Vector (T : Tuple) return Boolean;
-   function Make_Vector (X, Y, Z : Value) return Tuple;
-   function Make_Vector (X, Y, Z : Integer) return Tuple;
+   function Make_Vector (X, Y, Z : Value) return Tuple
+      with Post => Is_Vector (Make_Vector'Result);
+   function Make_Vector (X, Y, Z : Integer) return Tuple
+      with Post => Is_Vector (Make_Vector'Result);
 
    function Magnitude (T : Tuple) return Value;
    function Normalize (T : Tuple) return Tuple
@@ -43,7 +47,9 @@ package Radatracer is
    type Ray is record
       Origin : Tuple;
       Direction : Tuple;
-   end record;
+   end record
+      with Dynamic_Predicate => Is_Point (Origin) and Is_Vector (Direction);
 
-   function Position (R : Ray; T : Value) return Tuple;
+   function Position (R : Ray; T : Value) return Tuple
+      with Post => Is_Point (Position'Result);
 end Radatracer;
