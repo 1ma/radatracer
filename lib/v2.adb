1bfeb6ad1b84c704f6492b2576198cb8990d40b3
Marcel Hernandez <e6990620@gmail.com>
2021-04-18 18:09:24 +0200

    expand Radatracer v2

--- lib/v2.adb
+++ lib/v2.adb
@@ -1,9 +1,113 @@
+with Ada.Numerics.Generic_Elementary_Functions;
+
 package body V2 is
    overriding function "=" (L, R : Value) return Boolean is
       Epsilon : constant Value := 0.00001;
    begin
       return abs (L - R) < Epsilon;
    end "=";
+
+   overriding function "=" (L, R : Tuple) return Boolean is
+   begin
+      return L.X = R.X and L.Y = R.Y and L.Z = R.Z and L.W = R.W;
+   end "=";
+
+   function "+" (L, R : Tuple) return Tuple is
+   begin
+      return (
+         X => L.X + R.X,
+         Y => L.Y + R.Y,
+         Z => L.Z + R.Z,
+         W => L.W + R.W
+      );
+   end "+";
+
+   function "-" (L, R : Tuple) return Tuple is
+   begin
+      return (
+         X => L.X - R.X,
+         Y => L.Y - R.Y,
+         Z => L.Z - R.Z,
+         W => L.W - R.W
+      );
+   end "-";
+
+   function "-" (T : Tuple) return Tuple is
+   begin
+      return (
+         X => -T.X,
+         Y => -T.Y,
+         Z => -T.Z,
+         W => -T.W
+      );
+   end "-";
+
+   function "*" (L, R : Tuple) return Tuple is
+   begin
+      return (
+         X => L.X * R.X,
+         Y => L.Y * R.Y,
+         Z => L.Z * R.Z,
+         W => L.W * R.W
+      );
+   end "*";
+
+   function "*" (L : Tuple; R : Value) return Tuple is
+   begin
+      return (
+         X => L.X * R,
+         Y => L.Y * R,
+         Z => L.Z * R,
+         W => L.W * R
+      );
+   end "*";
+
+   function "*" (L : Value; R : Tuple) return Tuple is
+   begin
+      return (
+         X => R.X * L,
+         Y => R.Y * L,
+         Z => R.Z * L,
+         W => R.W * L
+      );
+   end "*";
+
+   function "/" (L : Tuple; R : Value) return Tuple is
+   begin
+      return (
+         X => L.X / R,
+         Y => L.Y / R,
+         Z => L.Z / R,
+         W => L.W / R
+      );
+   end "/";
+
+   function Make_Point (X, Y, Z : Integer) return Point is
+   begin
+      return (Value (X), Value (Y), Value (Z), 1.0);
+   end Make_Point;
+
+   function Make_Vector (X, Y, Z : Integer) return Vector is
+   begin
+      return (Value (X), Value (Y), Value (Z), 0.0);
+   end Make_Vector;
+
+   function Magnitude (V : Vector) return Value is
+      package Math is new Ada.Numerics.Generic_Elementary_Functions (Value);
+   begin
+      return Math.Sqrt ((V.X * V.X) + (V.Y * V.Y) + (V.Z * V.Z));
+   end Magnitude;
+
+   function Normalize (V : Vector) return Vector is
+      V_Magnitude : constant Value := Magnitude (V);
+   begin
+      return (
+         X => V.X / V_Magnitude,
+         Y => V.Y / V_Magnitude,
+         Z => V.Z / V_Magnitude,
+         W => V.W / V_Magnitude
+      );
+   end Normalize;
 
    function Dot_Product (L, R : Vector) return Value is
    begin
@@ -19,4 +123,9 @@ package body V2 is
          W => 0.0
       );
    end Cross_Product;
+
+   function Position (R : Ray; T : Value) return Point is
+   begin
+      return R.Origin + R.Direction * T;
+   end Position;
 end V2;
