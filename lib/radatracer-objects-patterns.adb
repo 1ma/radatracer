87cf0c9ed41d1ae49b947221b8207b4063e4e2c7
Marcel Hernandez <e6990620@gmail.com>
2021-06-20 17:35:19 +0200

    Implemented Ring pattern.
    This one also looks fucked, something's probably wrong with the
    Pattern_At_Object transformations.

--- lib/radatracer-objects-patterns.adb
+++ lib/radatracer-objects-patterns.adb
@@ -1,3 +1,5 @@
+with Ada.Numerics.Generic_Elementary_Functions;
+
 package body Radatracer.Objects.Patterns is
    overriding function Pattern_At (Self : Stripe; Point : Radatracer.Point) return Color is
    begin
@@ -13,5 +15,17 @@ package body Radatracer.Objects.Patterns is
       Fraction : constant Value := Point.X - Value'Floor (Point.X);
    begin
       return Self.A + (Distance * Fraction);
+   end Pattern_At;
+
+   overriding function Pattern_At (Self : Ring; Point : Radatracer.Point) return Color is
+      package Math is new Ada.Numerics.Generic_Elementary_Functions (Value);
+
+      Criteria : constant Value := Math.Sqrt (Point.X * Point.X + Point.Z * Point.Z);
+   begin
+      if Integer (Value'Floor (Criteria)) mod 2 = 0 then
+         return Self.A;
+      end if;
+
+      return Self.B;
    end Pattern_At;
 end Radatracer.Objects.Patterns;
--- lib/radatracer-objects-patterns.ads
+++ lib/radatracer-objects-patterns.ads
@@ -7,6 +7,11 @@ package Radatracer.Objects.Patterns is
        A, B : Color;
    end record;
 
+   type Ring is new Pattern with record
+       A, B : Color;
+   end record;
+
    overriding function Pattern_At (Self : Stripe; Point : Radatracer.Point) return Color;
    overriding function Pattern_At (Self : Gradient; Point : Radatracer.Point) return Color;
+   overriding function Pattern_At (Self : Ring; Point : Radatracer.Point) return Color;
 end Radatracer.Objects.Patterns;
